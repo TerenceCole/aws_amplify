221eff4204102648b98bcd1a929bd797767f54b5
Terence Cole <trcexcel@yahoo.com>
2020-09-02 12:05:50 -0500

    removed test data

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React on AWS Amplify - Terence Cole</h1>
-        <p>
-        <span>Design and Code by Terence Cole</span><br><a href="https://www.linkedin.com/in/terencecole/"
-          target="_blank"><i class="fab fa-linkedin"></i>
-          LinkedIn</a> | <a href="https://github.com/TerenceCole" target="_blank"><i class="fab fa-github"></i>
-          GitHub</a>
-        </p>
+        <h1>Hello from V2</h1>
       </header>
     </div>
   );
